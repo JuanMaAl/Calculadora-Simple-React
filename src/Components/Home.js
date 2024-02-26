9a6f6e051f43d4cced2b9fc0b6751ee40d0ef855
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2024-02-26 06:29:52 +0100

    Añadir la funcionalidad de la calculadora

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from "./Home.module.css"
 import Demo from './Demo.js'
 import Button from './Button.js'
 
 function Home() {
 
+  const [res, setRes] = useState("")
+
   const buttons = ["C", "9", "/", "8", "7", "6", "*", "5", "4", "3","+", "2", "1", "0", "-", ".", "Del", "="];
+
+  const findval = ()=>{
+    let result = Function("return "+res)();
+    setRes(result.toString());
+  }
+
+  const handler = (arg) => {
+    if(res === "Infinity"){
+      setRes("");
+      return;
+    }
+
+    if(arg == "C") setRes("");
+    else if(arg == "=") findval();
+    else if(arg == "Del"){
+      let n = res.length;
+      if (n>0)
+      setRes(res.slice(0, n-1)); 
+    }
+    else setRes(res.concat(arg))
+
+  }
 
   return (
     <div className={classes.home}>
         <div className={classes.inner}>
             <div className={classes.result}>
-                    <div className={classes.resbox}></div>
+                    <div className={classes.resbox}>
+                      {res}
+                    </div>
             </div>
           <div className={classes.btns}>
-            {buttons.map((ele,index)=> {return <Button value={ele} key={index}/>})}
+            {buttons.map((ele,index)=> {return <Button handler={handler} value={ele} key={index}/>})}
           </div>
         </div>
     </div>

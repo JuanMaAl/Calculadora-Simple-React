07b82e66c1ca18de953591cac578ceb86e09d7fa
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2024-02-26 05:27:01 +0100

    creación del componente Button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import classes from "./Home.module.css"
-import Demo from './Demo'
+import Demo from './Demo.js'
+import Button from './Button.js'
 
 function Home() {
+
+  const buttons = ["C", "9", "/", "8", "7", "6", "*", "5", "4", "3","+", "2", "1", "0", "-", ".", "Del", "="];
+
   return (
     <div className={classes.home}>
         <div className={classes.inner}>
             <div className={classes.result}>
                     <div className={classes.resbox}></div>
             </div>
+          <div className={classes.btns}>
+            {buttons.map((ele,index)=> {return <Button value={ele} key={index}/>})}
+          </div>
         </div>
-        <div className={classes.btns}></div>
     </div>
   )
 }
